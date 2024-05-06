69bf8d2faa8836c209628ed07f536df4dbc8ebfc
Fellype Bardales <lfpb.luiz@gmail.com>
2024-05-06 20:06:36 -0300

    Criação e customização das páginas restantes filmes, séries, contato e sobre.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,18 +17,44 @@ const topo = new Swiper("#topo", {
     type: "bullets",
     clickable: "false",
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  },
 });
 
 const personagens = new Swiper("#personagens", {
   speed: 800,
-  slidesPerView: 6,
   // autoplay: {
   //   delay: 8000,
   // },
-  spaceBetween: 10,
   navigation: {
     nextEl: ".btn-next",
     prevEl: ".btn-prev",
+  },
+  breakpoints: {
+    380: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
   },
 });
 
@@ -43,4 +69,16 @@ $(document).ready(function () {
   });
 });
 
-console.log(Fancybox);
+$(document).ready(function () {
+  // Quando clicar no ícone do menu
+  $(".icon-menu-hamburguer").click(function () {
+    // Mostra o menu
+    $(".menu-hamburguer").show();
+  });
+
+  // Quando clicar em Fechar Menu
+  $(".close-menu").click(function () {
+    // Esconde o menu
+    $(".menu-hamburguer").hide();
+  });
+});
