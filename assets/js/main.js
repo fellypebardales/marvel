5f531ddfd3d46dab7d151d7ff1c57925cc2b0837
Fellype Bardales <lfpb.luiz@gmail.com>
2024-05-06 13:48:59 -0300

    Criação de seção de filmes na home, footer e header.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,46 @@
 $(document).ready(function () {
-  $(".slick-banners").slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    fade: true,
-    cssEase: "linear",
-    arrows: false,
-  });
-
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
 });
+
+const topo = new Swiper("#topo", {
+  effect: "fade",
+  direction: "horizontal",
+  speed: 400,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 8000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: "false",
+  },
+});
+
+const personagens = new Swiper("#personagens", {
+  speed: 800,
+  slidesPerView: 6,
+  // autoplay: {
+  //   delay: 8000,
+  // },
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var rotation = scroll / 10; // Altere o valor conforme necessário para ajustar a rotação
+    var translateX = scroll; // Altere o valor conforme necessário para ajustar a velocidade de movimento
+    $(".martelo-thor").css({
+      transform: "translateX(" + translateX + "px) rotate(" + rotation + "deg)",
+    });
+  });
+});
+
+console.log(Fancybox);
